10b864d01daac340cc0f84aa4cef241b125dcca6
FherStk <porrino.fernando@gmail.com>
2025-12-05 01:37:08 +0100

    Storing changes into SQLite (incrementally).

--- run.py
+++ run.py
@@ -1,39 +1,175 @@
 import requests
+import sqlite3
+from datetime import datetime, timezone
 
+TOKEN_API = "<DMOJ ADMIN TOKEN API>"
+DB_NAME = "dmoj.db"
 STUDENTS = [
-    "username1",
-    "username2",
-    "..."
+    "admin", "fer"
 ]
+
+def db_init():
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS user (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT UNIQUE NOT NULL
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS tracking (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,            
+            date TEXT,
+            user_id INTEGER,
+            FOREIGN KEY(user_id) REFERENCES users(id)
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS submission (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,        
+            problem_id INTEGER,
+            problem_name TEXT,
+            date TEXT,
+            language TEXT,
+            time REAL,
+            memory REAL,
+            points INTEGER,
+            result TEXT,
+            contest TEXT,
+            tracking_id INTEGER,
+            FOREIGN KEY(tracking_id) REFERENCES tracking(id)
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+def get_or_create_user(user):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute("""
+              SELECT id 
+              FROM user u
+              WHERE u.name = ?
+          """, (user,))
+    result = cursor.fetchone()
+
+    if result:
+        conn.close()
+        return result[0]
+    else:
+        cursor.execute("""
+               INSERT INTO user (name)
+               VALUES (?)
+           """, (user,))
+        uid = cursor.lastrowid
+        conn.commit()
+        conn.close()
+        return uid
+
+def get_user_last_tracking(uid):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute(
+    """
+            SELECT max(date) 
+            FROM tracking        
+            WHERE user_id = ?
+        """, (uid,)
+   )
+    result = cursor.fetchone()
+
+    conn.commit()
+    conn.close()
+    return result[0] if (result and result[0]) else "1900-01-01"
+
+def get_user_last_submission(uid):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute(
+    """
+            SELECT max(s.date) 
+            FROM submission s 
+            LEFT JOIN tracking t ON s.tracking_id = t.id        
+            LEFT JOIN user u ON t.user_id = u.id
+            WHERE user_id = ?
+        """, (uid,)
+   )
+    result = cursor.fetchone()
+
+    conn.commit()
+    conn.close()
+    return result[0] if (result and result[0]) else None
+
+def create_tracking(uid):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute(
+    """
+           INSERT INTO tracking (date, user_id)
+           VALUES (?, ?)
+        """, (datetime.now(timezone.utc).isoformat(),uid,)
+    )
+    tid = cursor.lastrowid
+
+    conn.commit()
+    conn.close()
+    return tid
+
+def create_submission(tid, submit):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute(
+    """
+           INSERT INTO submission (problem_id, problem_name, date, language, time, memory, points, result, contest, tracking_id)
+           VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """, (
+            submit.get('id'), submit.get('problem'), submit.get('date'), submit.get('language'), submit.get('time'),
+            submit.get('memory'), submit.get('points'), submit.get('result'), submit.get('contest'), tid,
+        )
+    )
+
+    conn.commit()
+    conn.close()
 
 def get_submit_data_by_user(user):
     url = f"https://dmoj.elpuig.xeill.net/api/v2/submissions?user={user}"
 
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers={
+            "Authorization": f"Bearer {TOKEN_API}"
+        })
 
         if response.status_code == 200:
             content = response.json()
             submits = content.get("data", {}).get("objects", [])
 
-            if not submits: return {
+            uid = get_or_create_user(user)
+            last_tracking = get_user_last_tracking(uid)
+            tid = create_tracking(uid)
+
+            recent = [s for s in submits if s.get('date') and s['date'] > last_tracking]
+            for submit in recent:
+                create_submission(tid, submit)
+
+            if not recent: return {
                 "total": 0,
-                "last": None,
+                "last": get_user_last_submission(uid),
                 "error": None
             }
 
-            dates = [submit['date'] for submit in submits if submit['date'] is not None]
+            dates = [submit['date'] for submit in recent if submit['date'] is not None]
             return {
-                "total": len(submits),
+                "total": len(recent),
                 "last": max(dates),
                 "error": None
-            }
-
-        else:
-            return {
-                "total": 0,
-                "last": None,
-                "error": response.status_code
             }
 
     except requests.exceptions.RequestException as e:
@@ -44,8 +180,12 @@ def get_submit_data_by_user(user):
         }
 
 if __name__ == "__main__":
-    print("Requesting data for the suplied students. The output will be formated in order to copy/paste into a spreadsheet.\n")
-    print("USER\tTOTAL\tLAST")
+    db_init()
+
+    print("Requesting data for the suplied students.")
+    print("  - All data will be stored into a SQLite BBDD.")
+    print("  - The most recent data will de displayed and formatted to copy/paste it into a spreadsheet.\n")
+    print("USER\tNEW SUBMISSIONS\tLAST SUBMISSION")
 
     for user in STUDENTS:
         data = get_submit_data_by_user(user)
